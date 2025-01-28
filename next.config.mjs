ae6c49e7b20a15ca3c2516fa3d974fdda1098da5
Maham-Ahsan <160377217+Maham-Ahsan@users.noreply.github.com>
2025-01-28 19:26:56 +0500

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['images.sanity.io'],
+    }
+};
 
 export default nextConfig;
